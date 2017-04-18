984573224bb7334c321050a2397df3b4c246443e
npikaart <noah.pikaart.wgd@gmail.com>
2017-04-18 13:31:07 -0400

    Updated php

--- php/proscess.php
+++ php/proscess.php
@@ -75,34 +75,37 @@ if($fname != null && $lname != null && $email != null)
             $message = "An error occured while proscessing user input. Please verify the integrity of all scripts";
             @mail($admin, $subject, $message, $headers);
             
-        }
-
-        $subject = "Vendicant Games Trade Show";
-        $message = "Thanks for signing up! We'll send you a reminder several days before the event starts";
-
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            if(@mail($email, $subject,$message,$headers)) {
-                $_SESSION["submission_results"] = true;
-                echo "<script>window.location.href = '../index.php'</script>";
-                
-                    echo '<!DOCTYPE html>
-        <html>
-            <head>  
-                <meta charset = "utf-8" />
-                <meta name = "viewport" content = "width=device-width,initial-scale=1.0,minimum-scale=1.0"/>
-                <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
-                <title>Results</title>
-            </head>
+        } else {
+            $_SESSION["submission_results"] = true;
             
-            <body style="display:flex;flex-flow:row wrap;justify-content:center;align-items:center">
-                <div style="margin:0 auto;font-size:3rem;font-family:orbitron;">Your response was sucessfully recorded.
-                    <br/>
-                    <a style = "display: block; color: #ffffff; font-family: orbitron;font-size:1.25rem; margin:1rem auto; text-decoration: none; height: 40px; width: 130px; line-height: 40px;background-color: #1a1a1a; text-align: center;" href = "http://kuzmaclass.org/sandbox/VendicantGamesStoryboard1.10PM/contact.php">Back</a>
-                </div>
-            </body>
+            $subject = "Vendicant Games Trade Show";
+            $message = "Thanks for signing up! We'll send you a reminder several days before the event starts";
+
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                if(@mail($email, $subject,$message,$headers)) {
+
+                    echo "<script>window.location.href = '../index.php'</script>";
+
+                    echo '<!DOCTYPE html>
+                    <html>
+                        <head>  
+                            <meta charset = "utf-8" />
+                            <meta name = "viewport" content = "width=device-width,initial-scale=1.0,minimum-scale=1.0"/>
+                            <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+                            <title>Results</title>
+                        </head>
+
+                        <body style="display:flex;flex-flow:row wrap;justify-content:center;align-items:center">
+                            <div style="margin:0 auto;font-size:3rem;font-family:orbitron;">Your response was sucessfully recorded.
+                                <br/>
+                                <a style = "display: block; color: #ffffff; font-family: orbitron;font-size:1.25rem; margin:1rem auto; text-decoration: none; height: 40px; width: 130px; line-height: 40px;background-color: #1a1a1a; text-align: center;" href = "http://kuzmaclass.org/sandbox/VendicantGamesStoryboard1.10PM/contact.php">Back</a>
+                            </div>
+                        </body>
+
+                    </html>
+                    ';
+            }
         
-        </html>
-        ';
             } else {
                 $_SESSION["email_error"] = true;
                 echo "<script>window.location.href = '../index.php'</script>";
